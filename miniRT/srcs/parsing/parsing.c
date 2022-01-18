02a0e76dfa25384d51c89db98522f376baa3a88a
rcollas <rcollas@student.42.fr>
2022-01-18 18:25:15 +0100

    first checks on .rt file

--- miniRT/srcs/parsing/parsing.c
+++ miniRT/srcs/parsing/parsing.c
@@ -1,10 +1,34 @@
 #include "miniRT.h"
 
-_Bool	is_valid_extension(char *arg)
+_Bool	ft_strcmp(char *s1, char *s2)
 {
+	int i;
 
+	i = 0;
+	while (s1 && s1[i] && s2 && s2[i] && s1[i] == s2[i])
+	{
+		if (s1[i + 1] == 0 && s2[i + 1] == 0)
+			return (EQUAL);
+		i++;
+	}
+	return (DIFFERENT);
 }
 
-int parsing(char **argv)
+_Bool	is_valid_extension(char *arg)
 {
+	char *extension;
+
+	extension = ft_strnstr(arg, ".rt", ft_strlen(arg));
+	if (!extension || ft_strcmp(extension, ".rt") == DIFFERENT)
+		return (parsing_error(EXTENSION_ERROR, arg));
+	return (SUCCESS);
+}
+
+int parsing(char **argv, int argc)
+{
+	if (argc != 2)
+		return (parsing_error(ARG_NUMBER_ERROR, NULL));
+	if (is_valid_extension(argv[1]) == FALSE)
+		return (EXTENSION_ERROR);
+	return (SUCCESS);
 }
