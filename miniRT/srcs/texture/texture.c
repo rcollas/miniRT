33faf50116428abb9bd0cdd0481171b87accb5ad
EugenieF <eug.francon@gmail.com>
2022-03-11 00:13:11 +0100

    Disk uv_mapping improved

--- miniRT/srcs/texture/texture.c
+++ miniRT/srcs/texture/texture.c
@@ -15,6 +15,35 @@ void	create_texture(t_ray *hit, t_image *texture, t_vec3 *color)
 	color->coord[R] = (double)(unsigned char)texture->addr[i + 2] / 255;
 	color->coord[G] = (double)(unsigned char)texture->addr[i + 1] / 255;
 	color->coord[B] = (double)(unsigned char)texture->addr[i] / 255;
+	// if (color->coord[R] > 0.01 && color->coord[G] > 0.01 && color->coord[B] > 0.01)
+	// 	printf("%f  %f  %f\n", color->coord[R], color->coord[G], color->coord[B]);
+}
+
+void	mix_textures(t_ray *hit, t_image *texture_1, t_image *texture_2, t_vec3 *color)
+{
+	t_vec2	uv;
+	int		floor_uv_1[2];
+	int		floor_uv_2[2];
+	int		i;
+	int		j;
+
+	hit->obj->get_uv_coord(*hit, &uv);
+	uv.coord[U] = 1 - uv.coord[U];
+	uv.coord[V] = 1 - uv.coord[V];
+	floor_uv_1[U] = floor(uv.coord[U] * (texture_1->width - 1));
+	floor_uv_1[V] = floor(uv.coord[V] * (texture_1->height - 1));
+	floor_uv_2[U] = floor(uv.coord[U] * (texture_2->width - 1));
+	floor_uv_2[V] = floor(uv.coord[V] * (texture_2->height - 1));
+
+	i = floor_uv_1[V] * texture_1->line_len + floor_uv_1[U] * texture_1->bpp / 8;
+	j = floor_uv_2[V] * texture_2->line_len + floor_uv_2[U] * texture_2->bpp / 8;
+
+	color->coord[R] = (double)(unsigned char)texture_1->addr[i + 2] / 255;
+	color->coord[G] = (double)(unsigned char)texture_1->addr[i + 1] / 255;
+	color->coord[B] = (double)(unsigned char)texture_1->addr[i] / 255;
+	color->coord[R] /= ((double)(unsigned char)texture_2->addr[i + 2] / 255 - 0.03);
+	color->coord[G] /= ((double)(unsigned char)texture_2->addr[i + 1] / 255 - 0.03);
+	color->coord[B] /= ((double)(unsigned char)texture_2->addr[i] / 255 - 0.03);
 }
 
 void	handle_texture(t_ray *hit)
@@ -27,6 +56,7 @@ void	handle_texture(t_ray *hit)
 	{
 		create_texture(hit, hit->obj->texture, &hit->color);
 		apply_bump_map(hit);
+		// mix_textures(hit, hit->obj->texture, hit->obj->bump_map, &hit->color);
 	}
 }
 
--- miniRT/srcs/texture/uv_mapping.c
+++ miniRT/srcs/texture/uv_mapping.c
@@ -106,36 +106,90 @@ void	get_plane_uv(t_ray hit, t_vec2 *uv)
 // 	normalize_vec2(uv);
 // }
 
+// void	get_disk_uv(t_ray hit, t_vec2 *uv)
+// {
+// 	double	theta;
+// 	double	radius;
+// 	t_vec2	center;
+// 	t_vec2	norm_uv;
+// 	t_vec2	point;
+
+// 	point.coord[X] = hit.origin.coord[X];
+// 	point.coord[Y] = hit.origin.coord[Z];
+// 	normalize_vec2(&point);
+// 	// printf("x = %f | z = %f\n", point.coord[X], point.coord[Y]);
+// 	center.coord[X] = hit.obj->texture->center.coord[X];
+// 	center.coord[Y] = hit.obj->texture->center.coord[Y];
+// 	normalize_vec2(&center);
+// 	norm_uv.coord[X] = 2.0 * point.coord[X] - center.coord[X];
+// 	norm_uv.coord[Y] = 2.0 * point.coord[Y] - center.coord[Y];
+// 	radius = get_norm_vec2(norm_uv);
+// 	theta = atan2(norm_uv.coord[Y], norm_uv.coord[X]);
+
+// 	// if (radius > 0.0 && radius < 2.0)
+// 	// 	radius = ceil(radius / 0.9) * 0.9;
+// 	// if (theta > 0.0 && theta < 6.0)
+// 	// 	theta = floor(theta / 0.8) * 0.8;
+// 	radius = (radius < 0.0) ? radius : (radius > 0.0 + 2.0) ? radius : ceil(radius / 0.9) * 0.9;
+//     theta = (theta < 0.0) ? theta : (theta > 0.0 + 5.0) ? theta : floor(theta / 0.8) * 0.8;
+
+// 	norm_uv.coord[X] = radius * cos(theta);
+// 	norm_uv.coord[Y] = radius * sin(theta);
+// 	uv->coord[U] = norm_uv.coord[X] / 2.0 + (center.coord[X] / 2.0);
+// 	uv->coord[V] = norm_uv.coord[Y] / 2.0 + (center.coord[Y] / 2.0);
+// 	// printf("u = %f | v = %f\n", uv->coord[U], uv->coord[V]);
+// }
+
+t_vec2	transform_point_in_obj_space(t_vec3 hit_point, t_vec3 obj_origin, double max_dimension)
+{
+	t_vec2	new_point;
+
+	new_point.coord[X] = (hit_point.coord[X] - obj_origin.coord[X]) / max_dimension;
+	new_point.coord[Y] = (hit_point.coord[Z] - obj_origin.coord[Z]) / max_dimension;
+	return (new_point);
+}
+
 void	get_disk_uv(t_ray hit, t_vec2 *uv)
 {
 	double	theta;
 	double	radius;
-	t_vec2	center;
-	t_vec2	norm_uv;
 	t_vec2	point;
 
-	point.coord[X] = hit.origin.coord[X];
-	point.coord[Y] = hit.origin.coord[Z];
-	normalize_vec2(&point);
-	// printf("x = %f | z = %f\n", point.coord[X], point.coord[Y]);
-	center.coord[X] = hit.obj->texture->center.coord[X];
-	center.coord[Y] = hit.obj->texture->center.coord[Y];
-	normalize_vec2(&center);
-	norm_uv.coord[X] = 2.0 * point.coord[X] - center.coord[X];
-	norm_uv.coord[Y] = 2.0 * point.coord[Y] - center.coord[Y];
-	radius = get_norm_vec2(norm_uv);
-	theta = atan2(norm_uv.coord[Y], norm_uv.coord[X]);
-
-	// if (radius > 0.0 && radius < 2.0)
-	// 	radius = ceil(radius / 0.9) * 0.9;
-	// if (theta > 0.0 && theta < 6.0)
-	// 	theta = floor(theta / 0.8) * 0.8;
-	radius = (radius < 0.0) ? radius : (radius > 0.0 + 2.0) ? radius : ceil(radius / 0.9) * 0.9;
-    theta = (theta < 0.0) ? theta : (theta > 0.0 + 5.0) ? theta : floor(theta / 0.8) * 0.8;
-
-	norm_uv.coord[X] = radius * cos(theta);
-	norm_uv.coord[Y] = radius * sin(theta);
-	uv->coord[U] = norm_uv.coord[X] / 2.0 + (center.coord[X] / 2.0);
-	uv->coord[V] = norm_uv.coord[Y] / 2.0 + (center.coord[Y] / 2.0);
+	point = transform_point_in_obj_space(hit.origin, *hit.obj->origin, hit.obj->diameter / 2.0);
+	// printf("x = %f | y = %f\n", point.coord[X], point.coord[Y]);
+	radius = get_norm_vec2(point);
+	// printf("radius = %f\n", radius);
+	theta = atan2(point.coord[Y], point.coord[X]);
+	uv->coord[U] = (radius - 0.25) / (0.5 - 0.25);
+	uv->coord[V] = theta * 0.5 / M_PI + 0.5;
 	// printf("u = %f | v = %f\n", uv->coord[U], uv->coord[V]);
 }
+
+// void	get_disk_uv(t_ray hit, t_vec2 *uv)
+// {
+// 	double	theta;
+// 	double	radius;
+// 	t_vec2	point;
+// 	t_vec2	center;
+// 	t_vec2	norm_point;
+
+// 	point = transform_point_in_obj_space(hit.origin, hit.obj);
+// 	// printf("x = %f | y = %f\n", point.coord[X], point.coord[Y]);
+// 	// printf("radius = %f\n", radius);
+// 	center.coord[X] = hit.obj->texture->center.coord[X];
+// 	center.coord[Y] = hit.obj->texture->center.coord[Y];
+// 	normalize_vec2(&center);
+// 	norm_point.coord[X] = 2.0 * point.coord[X] - center.coord[X];
+// 	norm_point.coord[Y] = 2.0 * point.coord[Y] - center.coord[Y];
+
+// 	radius = get_norm_vec2(norm_point);
+// 	theta = atan2(norm_point.coord[Y], norm_point.coord[X]);
+// 	radius = (radius < 0.0) ? radius : (radius > 0.0 + 2.0) ? radius : ceil(radius / 1) * 1;
+//     theta = (theta < 0.0) ? theta : (theta > 0.0 + 5.0) ? theta : floor(theta / 1) * 1;
+
+// 	norm_point.coord[X] = radius * cos(theta);
+// 	norm_point.coord[Y] = radius * sin(theta);
+// 	uv->coord[U] = norm_point.coord[X] / 2.0 + (center.coord[X] / 2.0);
+// 	uv->coord[V] = norm_point.coord[Y] / 2.0 + (center.coord[Y] / 2.0);
+// 	printf("u = %f | v = %f\n", uv->coord[U], uv->coord[V]);
+// }
