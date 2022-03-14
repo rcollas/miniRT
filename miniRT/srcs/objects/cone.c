ac8b6b415efb7764fc01eb58bddc6c30f7a18fed
EugenieF <eug.francon@gmail.com>
2022-03-15 00:20:46 +0100

    Cone better with moves, prob light base

--- miniRT/srcs/objects/cone.c
+++ miniRT/srcs/objects/cone.c
@@ -5,25 +5,13 @@ void	check_normal_cone(t_ray *ray, t_obj *obj, t_ray *hit)
 	(void)obj;
 	(void)hit;
 	(void)ray;
-	// if (dot_vec3(hit->dir, ray->dir) > 0)
-	// {
-		// hit->dir = mul_vec3_and_const(hit->dir, -1);
+	if (dot_vec3(hit->dir, ray->dir) > 0)
+	{
+		hit->dir = mul_vec3_and_const(hit->dir, -1);
 		// if (BONUS && obj->type == SPHERE)
-			obj->inside_object = FALSE;
+			// obj->inside_object = FALSE;
+	}
 }
-
-// void	compute_normal_cone(t_ray *ray, t_obj *obj, t_ray *hit, double angle)
-// {
-// 	double	m;
-// 	double	radius;
-
-// 	radius = obj->diameter / 2.0;
-// 	m = dot_vec3(ray->dir, *obj->dir) * hit->dist;
-// 	m += dot_vec3(sub_vec3(ray->origin, *obj->origin), *obj->dir);
-// 	hit->dir = sub_vec3(sub_vec3(hit->origin, *obj->origin), mul_vec3_and_const(*obj->dir, (1 + tan(angle) * tan(angle)) * m));
-// 	normalize_vec3(&hit->dir);
-// 	check_normal_cone(ray, obj, hit);
-// }
 
 void	compute_normal_cone(t_ray *ray, t_obj *obj, t_ray *hit)
 {
@@ -66,6 +54,32 @@ _Bool	hit_cone_caps(t_ray *ray, t_obj *obj, t_ray *hit)
 	return (FALSE);
 }
 
+_Bool	solve_quadratic_cone(
+	double *coeff, double *roots, double *closest_hit)
+{
+	double	delta;
+	// double	tmp;
+
+	delta = coeff[B] * coeff[B] - (4 * coeff[A] * coeff[C]);
+	if (delta < 0)
+		return (0);
+	roots[0] = (-coeff[B] - (sqrt(delta))) / (2 * coeff[A]);
+	roots[1] = (-coeff[B] + (sqrt(delta))) / (2 * coeff[A]);
+	// if (roots[0] < roots[1])
+	// {
+	// 	tmp = roots[0];
+	// 	roots[0] = roots[1];
+	// 	roots[1] = tmp;
+	// }
+	if (roots[0] > 0)
+		*closest_hit = roots[0];
+	else if (roots[1] > 0)
+		*closest_hit = roots[1];
+	else
+		return (FALSE);
+	return (TRUE);
+}
+
 _Bool	hit_cone(t_ray *ray, t_obj *obj, t_ray *hit)
 {
 	double	coeff[3];
@@ -86,13 +100,12 @@ _Bool	hit_cone(t_ray *ray, t_obj *obj, t_ray *hit)
 	coeff[A] = dot_vec3(ray->dir, ray->dir) - angle * s_d1 * s_d1 - s_d1 * s_d1;
 	coeff[B] = 2 * (dot_vec3(ray->dir, obj_to_origin) - angle * s_d1 * s_d2 - s_d1 * s_d2);
 	coeff[C] = dot_vec3(obj_to_origin, obj_to_origin) - angle * s_d2 * s_d2 - s_d2 * s_d2;
-	if (solve_quadratic_equation(coeff, roots, &ray->dist) && ray->dist > 0.05
+	if (solve_quadratic_cone(coeff, roots, &ray->dist) && ray->dist > 0.05
 		&& check_height(ray, obj))
 	{
 		hit->origin = get_hit_point(*ray);
 		hit->dist = ray->dist;
 		compute_normal_cone(ray, obj, hit);
-		// compute_normal_cone(ray, obj, hit, angle);
 		return (TRUE);
 	}
 	if (hit_cone_caps(ray, obj, hit))
